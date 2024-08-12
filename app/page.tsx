339e912098305060634fc1575e8403e6711b032f
Messias Neto <messias@diferencialtech.com.br>
2024-08-12 09:07:05 -0300

    feat: add PhoneItem component and Footer on layout

--- app/page.tsx
+++ app/page.tsx
@@ -74,16 +74,6 @@ const Home = async () => {
             />
           ))}
         </div>
-
-        <footer className="mt-6">
-          <Card>
-            <CardContent className="px-5 py-6">
-              <p className="text-sm text-gray-400">
-                © 2023 Copyright <span className="font-bold">FSW Barber</span>
-              </p>
-            </CardContent>
-          </Card>
-        </footer>
       </div>
     </div>
   )
